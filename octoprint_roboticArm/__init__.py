5904f9a55aa855843954674f40bc28ce4bff11e9
Ryan Schulman <schulmanworks@gmail.com>
2016-03-08 18:54:37 -0700

    another commit

--- octoprint_roboticArm/__init__.py
+++ octoprint_roboticArm/__init__.py
@@ -6,9 +6,7 @@ class RoboticArm(octoprint.plugin.StartupPlugin):
 	def on_after_startup(self):
 		self._logger.info("Hello World!")
     
-__plugin_name__ = "Robotic Arm"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "Robotic Arm Control for Octoprint"
+
 __plugin_implementation__ = RoboticArm()
 
 
